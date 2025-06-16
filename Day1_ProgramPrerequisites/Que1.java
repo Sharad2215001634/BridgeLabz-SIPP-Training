0ac445fe3af624401924cb83d5f54a18db954cac
Sharad <sharadkumars035@gmail.com>
2025-06-16 13:17:38 +0530

    [Sharad] Programimg Preequisites Are Added

---
+++ Day1_ProgramPrerequisites/Que1.java
@@ -0,0 +1,8 @@
+package Day1_ProgramPrerequisites;
+class Que1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Bridgelabz!");
+    }
+
+    
+}
---
+++ Day1_ProgramPrerequisites/Que2.java
@@ -0,0 +1,10 @@
+package Day1_ProgramPrerequisites;
+
+public class Que2 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println(a + b);
+    }
+
+}
